357aebd2a314b7ccc27abd8eaa299c834bc5e86b
mohamed-jilani <jilanimed07@gmail.com>
2024-11-25 16:09:51 +0100

    create the project structure

---
+++ frontend/src/home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = (props) => {
+  const { loggedIn, email } = props
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    if (loggedIn) {
+      // Déconnexion
+      props.setLoggedIn(false)
+      props.setEmail('')
+    } else {
+      // Redirection vers la page de login
+      navigate('/login')
+    }
+  }
+  
+
+  return (
+    <div className="mainContainer">
+      <div className={'titleContainer'}>
+        <div>Welcome!</div>
+      </div>
+      <div>This is the home page.</div>
+      <div className={'buttonContainer'}>
+        <input
+          className={'inputButton'}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? 'Log out' : 'Log in'}
+        />
+        {loggedIn ? <div>Your email address is {email}</div> : <div />}
+      </div>
+    </div>
+  )
+}
+
+export default Home
